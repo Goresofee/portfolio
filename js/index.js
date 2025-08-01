16ed6b6494aa339b0a6384a3c1d055c433bd64d4
Goresofee <gorelikovason@gmail.com>
2025-08-01 11:54:01 +0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -4,3 +4,20 @@ const nav = document.querySelector('.nav');
 burger.addEventListener('click', () => {
   nav.classList.toggle('active');
 });
+
+
+const previewBlock = document.getElementById("floating-image");
+const previewImg = previewBlock.querySelector("img");
+
+document.querySelectorAll(".hover-word").forEach(word => {
+  word.addEventListener("mouseenter", () => {
+    const imgSrc = word.dataset.img;
+    previewImg.src = imgSrc;
+    previewBlock.style.display = "block";
+  });
+
+  word.addEventListener("mouseleave", () => {
+    previewBlock.style.display = "none";
+    previewImg.src = "";
+  });
+});
